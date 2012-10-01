0114f278a730526e712628888c37068b67753143
Vojtech Dobes <me@vojtechdobes.com>
2012-10-01 08:52:40 +0200

    Turned to compiler extension + removed Factory [BC break!]

---
+++ ReCaptchaExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+
+/**
+ * Nette\Forms reCAPTCHA compiler extension
+ *
+ * @author kesspess
+ */
+class ReCaptchaExtension extends Nette\Object
+{
+
+	/** @var string[] */
+	private $defaults = array(
+		'methodName' => 'addReCaptcha',
+	);
+
+
+
+	public function loadConfiguration()
+	{
+		$container = $this->getContainerBuilder();
+		$config = $this->getConfig();
+
+		$container->addDefinition($this->prefix('recaptcha'))
+			->setClass('ReCaptcha\ReCaptcha', array(
+				$config['publicKey'], $config['privateKey'],
+			));
+	}
+
+
+
+	public function afterCompile(Nette\Utils\PhpGenerator\ClassType $class)
+	{
+		$initialize = $class->methods['initialize'];
+		$container = $this->getContainerBuilder();
+		$config = $this->getConfig($this->defaults);
+ 
+		$initialize->addBody('$context = $this;');
+		$initialize->addBody('Nette\Forms\Container::extensionMethod(?, function ($container, $name, $label = NULL) use ($context) {
+			return $container[$name] = new ReCaptchaControl($context->getService(?), $context->getByType(\'Nette\Http\IRequest\'), $label);
+		});', array($config['methodName'], $this->prefix('recaptcha')));
+	}
+
+}
