385e18372518dfd25fcbe37461761eb868925a85
Petr Kessler <kesspess@gmail.com>
2018-05-26 10:37:23 +0200

    tests: added helper function dd()

--- tests/tester/bootstrap.php
+++ tests/tester/bootstrap.php
@@ -4,3 +4,8 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/keys.php';
 
 Tester\Environment::setup();
+
+function dd($var) {
+	array_map('dump', func_get_args());
+	exit(1);
+}
