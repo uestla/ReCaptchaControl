6de343d831c56d2e3ed32373952669dc30f1b340
Petr Kessler <kesspess@gmail.com>
2018-05-27 11:32:51 +0200

    tests: extracted autoloading & helper functions

---
+++ tests/tester/autoload.php
@@ -0,0 +1,34 @@
+<?php
+
+use Tester\Helpers;
+use Nette\DI\Container;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/DI/CustomRequester.php';
+
+
+// === helpers =========================
+
+function dd($var)
+{
+	array_map('dump', func_get_args());
+	exit(1);
+}
+
+
+/** @return Container */
+function createContainer($config = null)
+{
+	$configurator = new Nette\Configurator;
+
+	$tempDir = __DIR__ . '/temp/' . getmypid();
+
+	Helpers::purge($tempDir);
+	$configurator->setTempDirectory($tempDir);
+
+	if ($config) {
+		$configurator->addConfig($config);
+	}
+
+	return $configurator->createContainer();
+}
