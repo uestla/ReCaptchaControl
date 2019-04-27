1c231431b12855fc7dcd4bdc07a156747f4ef99a
Petr Kessler <kesspess@gmail.com>
2019-04-27 19:08:00 +0200

    tests/manual: library loaded directly via loader.php

--- tests/manual/app/bootstrap.php
+++ tests/manual/app/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../../src/loader.php';
 
 $configurator = new Nette\Configurator;
 
