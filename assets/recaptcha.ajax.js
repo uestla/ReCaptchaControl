0ba86c1bc3fe60c32864a166e48ba4719d6813b5
Ciki <matula.m@gmail.com>
2022-10-24 19:25:22 +0200

    Add support for AJAX and Invisible recaptcha
    thanks to https://gist.github.com/tvaliasek/83a166e5c773cece3b5c7d49a964e030

--- assets/recaptcha.ajax.js
+++ assets/recaptcha.ajax.js
@@ -1,27 +1,61 @@
 /**
  * This file is part of the ReCaptchaControl package
+ * AJAX with invisible recaptcha support thanks to https://gist.github.com/tvaliasek/83a166e5c773cece3b5c7d49a964e030
  *
  * @license  MIT
  * @author   Petr Kessler (https://kesspess.cz)
  * @link     https://github.com/uestla/ReCaptchaControl
  */
 
-;(function (window, $) {
+;
+(function (window, $, useInvisible) {
 
 	if (!$ || typeof $.nette === 'undefined') {
 		console.error('nette.ajax.js library is required, please load it.');
 	}
+
+	// map of <htmlID>: <widget/client ID>
+	var clientIDs = {};
 
 	// renders all recaptcha elements on page
 	var renderAll = function () {
 		$('.g-recaptcha').each(function () {
 			var el = $(this);
 			if (el.children().length) { // already rendered -> skip
-				return ;
+				return;
 			}
 
-			grecaptcha.render(this, {}, true);
+			var params = {};
+			if (useInvisible) {
+				// https://developers.google.com/recaptcha/docs/invisible#render_param
+				params = {
+					size: 'invisible',
+					// badge: 'inline',
+					callback: function (token) {
+//						el.closest('form').off('submit.recaptcha').trigger('submit');
+						// submit form using first button
+						el.closest('form').off('submit.recaptcha').find('input[type="submit"]:first').trigger('click');
+					}
+				};
+			}
+			clientIDs[this.id] = grecaptcha.render(this, params, true);
 		});
+
+		if (useInvisible) {
+			$(function () {
+				$('form').on('submit.recaptcha', function (event) {
+					event.preventDefault();
+
+					var form = $(this);
+					if (Nette.validateForm(this, true)) {
+						// execute only reCAPTCHAs in submitted form
+						$('.g-recaptcha', form).each(function () {
+							grecaptcha.execute(clientIDs[this.id]);
+						});
+					}
+				});
+			});
+		}
 	};
 
 	// set global onload callback (used in library loading below)
@@ -33,6 +67,38 @@
 		$.nette.ext('recaptcha', {
 			load: function () {
 				renderAll();
+			},
+			before: function (jqXHR, settings) {
+				if ((settings.hasOwnProperty('data')) && (settings.data instanceof FormData)) {
+					var formData = settings.data;
+					var formDataKeys = formData.keys();
+					var iterationDone = false;
+					var formDataKeysArray = [];
+					while (!iterationDone) {
+						try {
+							var keyItem = formDataKeys.next();
+							iterationDone = keyItem.done;
+							if (!iterationDone) {
+								formDataKeysArray.push(keyItem.value);
+							}
+						} catch (error) {
+							iterationDone = true
+						}
+					}
+					for (var index in formDataKeysArray) {
+						if (formDataKeysArray[index] === 'g-recaptcha-response') {
+							var formDataValue = formData.get(formDataKeysArray[index]);
+							if (formDataValue === '' || formDataValue === undefined) {
+								var formEl = settings.nette.form;
+								formEl.trigger('submit.recaptcha')
+								return false;
+							}
+						}
+					}
+				}
+			// },
+			// complete: function () {
+			// 	renderAll();
 			}
 		});
 	};
@@ -40,10 +106,12 @@
 	// load official library with explicit rendering
 	$('<script />', {
 		src: 'https://www.google.com/recaptcha/api.js'
-			+ '?onload=' + callbackName
-			+ '&render=explicit'
-			+ '&hl=' + ($('html').attr('lang') || 'en')
+				+ '?onload=' + callbackName
+				+ '&render=explicit'
+				+ '&hl=' + ($('html').attr('lang') || 'en'),
+		async: true,
+		defer: true
 
 	}).insertBefore('script:first');
 
-})(window, window.jQuery || false);
+})(window, window.jQuery || false, useInvisible = true);
